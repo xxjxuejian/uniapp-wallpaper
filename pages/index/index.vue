86cb47614d5bb3e4b9ef9a9642b87d0c02ed53f1
xu xuejian <2498492824@qq.com>
2025-08-27 20:32:57 +0800

    创建并初始化项目

---
+++ pages/index/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <view class="home">哈哈哈</view>
+</template>
+
+<script setup>
+const count = ref(0);
+console.log("count", count.value);
+console.log(11);
+</script>
+
+<style lang="scss" scoped>
+.home {
+  color: $text-color-main;
+}
+</style>
